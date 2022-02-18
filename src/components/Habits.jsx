cc109dec9f9c319431d79506f3c21ba4d29bb9aa
yogjin <youngjin988@gmail.com>
2022-02-18 17:35:45 +0900

    Habit key 설정

--- src/components/Habits.jsx
+++ src/components/Habits.jsx
@@ -4,16 +4,16 @@ import Habit from './Habit';
 class Habits extends Component {
   state = {
     habits: [
-      { name: '책읽기', count: 0 },
-      { name: '코딩', count: 1 },
-      { name: '운동', count: 0 },
+      { id: 1, name: '책읽기', count: 0 },
+      { id: 2, name: '코딩', count: 1 },
+      { id: 3, name: '운동', count: 0 },
     ],
   };
   render() {
     return (
       <ul>
         {this.state.habits.map((habit) => (
-          <Habit habit={habit} />
+          <Habit key={habit.id} habit={habit} />
         ))}
       </ul>
     );
